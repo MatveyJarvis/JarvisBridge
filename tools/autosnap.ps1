1117177a8fc80fe54f25174d51e5c4ba41da4a44
Matvey Jarvis <matvey@example.com>
2025-10-31 20:42:01 +0300

    AutoSnapshot - 2025-10-31_20-42-01

---
+++ tools/autosnap.ps1
@@ -0,0 +1,7 @@
+﻿Set-Location C:\JarvisBridge
+git add -A | Out-Null
+$changes = git status --porcelain
+if ($changes) {
+    $ts = Get-Date -Format 'yyyy-MM-dd_HH-mm-ss'
+    git commit -m "AutoSnapshot - $ts" | Out-Null
+}
